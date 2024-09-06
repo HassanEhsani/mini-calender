7389f64f518dfbc39dda88a355be0976c911c110
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-06 13:53:19 +0300

    wrote the code js in the js file about the get date online

---
+++ index.js
@@ -0,0 +1,16 @@
+const monthNameE1 = document.getElementById("month-name");
+const dayNameE1 = document.getElementById("day-name");
+const dayNumE1 = document.getElementById("day-Number");
+const yearE1 = document.getElementById("year");
+
+const date = new Date();
+monthNameE1.innerText = date.toLocaleString("en",{
+    month: "long"
+})
+
+dayNameE1.innerText = date.toLocaleString("en",{
+    weekday: "long"
+})
+
+dayNumE1.innerText = date.getDate();
+yearE1.innerText = date.getFullYear();
